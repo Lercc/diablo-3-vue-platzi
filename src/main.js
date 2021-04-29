2f04eb7e3d1d799ce12efde8b8740ee0cd094230
lercc <lercc.en@gmail.com>
2021-04-29 01:12:12 -0500

    agregando archivo de conf de prettier y instalación de boostrap-vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+// Paquetes de npm
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 
+// Archivos locales de la App
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+
+// CSS global
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Configuración extra
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
+// Instacia principal de Vue
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: () => import(/* webpackChunkName: "home" */ "@/views/Home")
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "home" */ '@/views/Home')
   },
   {
-    path: "/about",
-    name: "About",
-    component: () => import(/* webpackChunkName: "about" */ "@/views/About")
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About')
   }
 ];
 
