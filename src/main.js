39233f5315b3f2384bfe1bd8873dbcde23f91cf6
lercc <lercc.en@gmail.com>
2021-08-08 15:42:37 -0500

    refactorizando main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 // Paquetes de npm
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 
-// Font Awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-// iconos del tipo solid
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-// iconos del tipo brand (marcas o logos de empresas)
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-// componente a utilizar
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// BootstrapVue
+import '@/plugins/bootstrapVue'
+
+// Vue Font-Awesome
+import '@/plugins/fontAwesome'
 
 // Archivos locales de la App
 import App from '@/App.vue'
@@ -17,29 +13,10 @@ import router from '@/router'
 import store from '@/store'
 
 // CSS global
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/css/main.css'
 
-// Le añadimos los iconos que acabamos de importar (todos, los de tipo solid y de tipo brand)
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
 // Configuración extra
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // Instacia principal de Vue
 new Vue({
